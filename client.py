ab7031aa5482f57c67aeb63b5d7da9ba1886c59c
Adriaan Veney <45220404+aveney@users.noreply.github.com>
2022-03-02 09:30:56 -0500

    Add files via upload

---
+++ client.py
@@ -0,0 +1,147 @@
+import socket  # Import socket module
+from threading import *
+import os
+import glob
+import sys
+import platform
+import datetime
+
+quit_flag = False
+
+
+# to listen to any peers requiring uploads
+def upload_server():
+    upload_socket = socket.socket()
+    upload_host = socket.gethostbyname(socket.gethostname())
+    upload_port = port
+    upload_socket.bind((upload_host, upload_port))
+    upload_socket.listen(3)
+    peer_thread = current_thread()
+    while (quit_flag != True):
+        (client_socket, client_addr) = upload_socket.accept()
+        print('Got connection from', client_addr)
+        new_thread = Thread(target=peer_connection, args=(client_socket,))
+        new_thread.start()
+        new_thread.join()
+        print("What do you want to do?Enter number corresponding to an option you choose")
+        print("1. Add RFC's")
+        print("5. Quit")
+    upload_socket.close()
+
+
+# peer connection
+def peer_connection(client_socket):
+    data = client_socket.recv(1024).decode()
+    print('new request from peer')
+    data_list = data.split('\n')
+    for line in data_list:
+        print(line)
+    rfc = data_list[0].split(' ')[2]
+    file_name = "rfc" + str(rfc) + '.txt'
+    file_list = glob.glob('*.txt')
+    if file_name in file_list:
+        # mtime = os.path.getmtime(file_name)
+        # last_modified_date = datetime.datetime.fromtimestamp(mtime)
+        f_size = os.path.getsize(file_name)
+        response = "P2P-CI/1.0 200 OK\n" + "Date: " + str(datetime.datetime.now().strftime(
+            "%A, %d. %B %Y %I:%M%p")) + '\nOS: ' + platform.system() + '\n' + 'Content-Length: ' + str(
+            f_size) + '\n' + 'Content-Type: text/text'
+        client_socket.send(response.encode())
+        with open(file_name, 'r') as f:
+            file_data = f.read(1024).encode()
+            client_socket.send(file_data)
+            while file_data != "":
+                file_data = f.read(1024).encode()
+                client_socket.send(file_data)
+    else:
+        response = "P2P-CI/1.0 404 Not Found"
+        client_socket.send(response.encode())
+    client_socket.close()
+    print("What do you want to do?Enter number corresponding to an option you choose")
+    print("1. Add RFC's")
+    print("5. Quit")
+
+
+# to send requests to the central server_host
+def send_requests(note, server_host, server_port):
+    client_socket = socket.socket()
+    client_socket.connect((server_host, server_port))
+    client_socket.send(note.encode())
+    response = client_socket.recv(1024).decode()
+    print('response from the central server :')
+    print(response)
+    client_socket.close()
+
+
+# to send download requests to other peers
+def peer_requests(note, peer_host, peer_port, rfc):
+    client_socket = socket.socket()
+    client_socket.connect((peer_host, peer_port))
+    client_socket.send(note.encode())
+    response = client_socket.recv(1024).decode()
+    print('response from the peer :')
+    print(response)
+    data_list = response.split('\n')
+    if data_list[0].split(' ')[1] == '200':
+        name = "rfc" + str(rfc) + ".txt"
+        f = open(name, 'wb')
+        while (True):
+            d = client_socket.recv(1024)
+            if d:
+                f.write(d)
+                break
+            else:
+                f.close()
+                print('downloaded file sucessfully')
+                break
+    else:
+        print('file name not found')
+    client_socket.close()
+
+
+# handle quitting
+def quit(server_host, server_port):
+    note = "EXIT P2P-CI/1.0\nHost: " + host + '\n' + "Port: " + str(port)
+    send_requests(note, server_host, server_port)
+    quit_flag = True
+
+
+# to handle any user input
+def handle_input():
+    server_host = input("enter the host of the central server: ")
+    server_port = 7734
+    # for the first time a peer joins the system
+    note = "JOIN P2P-CI/1.0\nHost: " + host + '\n' + "Port: " + str(port)
+    send_requests(note, server_host, server_port)
+    file_list = glob.glob('*.txt')
+    # for file in file_list:
+    #     title = file.split('.')[0]
+    #     rfc = int(title.split('c')[1])
+    #     note = "ADD RFC " + str(rfc) + " P2P-CI/1.0\nHost: " + host + '\n' + "Port: " + str(
+    #         port) + '\n' + "Title: " + title
+    #     send_requests(note, server_host, server_port)
+    # after the client joins sucessfully give functionality options
+    while (True):
+        print("What do you want to do?Enter number corresponding to an option you choose")
+        print("1. Add RFC's")
+        print("5. Quit")
+        option = int(input())
+        if (option == 1):
+            add_rfc(server_host, server_port)
+        elif (option == 5):
+            quit(server_host, server_port)
+            break
+        else:
+            print('please enter a valid choice')
+
+
+# main client functionality
+host = input("please enter an unused host name: ")
+port = int(input("please enter an unused port number: "))
+upload_thread = Thread(target=upload_server)
+# destroy this upload thread on quitting
+upload_thread.daemon = True
+upload_thread.start()
+
+# now handle user input
+handle_input()
---
+++ server.py
@@ -0,0 +1,177 @@
+import socket  # Import socket module
+from threading import Thread
+
+
+###########################################LINKED LISTS FEATURES BEGINS################################################
+# Node for Linked Lists:
+# Class that models nodes in a linked list
+class Node:
+    def __init__(self, initdata):
+        self.data = initdata
+        self.next = None
+
+    def getData(self):
+        return self.data
+
+    def getNext(self):
+        return self.next
+
+    def setData(self, newdata):
+        self.data = newdata
+
+    def setNext(self, newnext):
+        self.next = newnext
+
+
+# LinkedLists and it's methods
+# class that models a linked list
+class LinkedList:
+
+    def __init__(self):
+        self.head = None
+
+    # method to iterate through all the items of a linked list
+
+    def __iter__(self):
+        current = self.head
+        while current is not None:
+            yield current
+            current = current.next
+
+    # method to add iterms to the linked list
+
+    def add(self, item):
+        temp = Node(item)
+        temp.setNext(self.head)
+        self.head = temp
+
+    # method to obtain the size of a linked list
+
+    def size(self):
+        current = self.head
+        count = 0
+        while current != None:
+            count = count + 1
+            current = current.getNext()
+
+        return count
+
+    # method to search a linked list
+
+    def search(self, item):
+        current = self.head
+        found = False
+        while current != None and not found:
+            if current.getData() == item:
+                found = True
+            else:
+                current = current.getNext()
+
+        return found
+
+    # method to remove a particular item from the linked list
+
+    def remove(self, item):
+        current = self.head
+        previous = None
+        found = False
+        while not found:
+            if current.getData() == item:
+                found = True
+            else:
+                previous = current
+                current = current.getNext()
+
+        if previous == None:
+            self.head = current.getNext()
+        else:
+            previous.setNext(current.getNext())
+
+
+# create a new linked list for active peers
+active_peers = LinkedList()
+
+# create a new linked list for index of RFC's
+rfc_index = LinkedList()
+
+
+#########################################LINKED LISTS FEATURES ENDS#####################################################
+
+# Active peers item:
+class PeerItem:
+    def __init__(self, peer_host, peer_port):
+        self.peer_host = peer_host
+        self.peer_port = peer_port
+
+
+# RFC index item:
+class RFCItem:
+    def __init__(self, rfc_number, rfc_title, rfc_host):
+        self.rfc_number = rfc_number
+        self.rfc_title = rfc_title
+        self.rfc_host = rfc_host
+
+
+# Retain a list of active peers
+peer_list = []
+
+# handle new peers joining
+def client_join(data_list, client_socket):
+    host = data_list[1].split(':')[1]
+    port = data_list[2].split(':')[1]
+    peer_list.append(host + ':' + port)
+    print(peer_list)
+    client_socket.send('You have sucessfully joined the P2P network'.encode())
+
+# client exit
+def client_exit(data_list, client_socket):
+    host = data_list[1].split(':')[1]
+    port = data_list[2].split(':')[1]
+    print('host {0} at port {1} is quitting'.format(host, port))
+    for item in rfc_index:
+        if host == item.getData().rfc_host:
+            rfc_index.remove(item.getData())
+    exiting_peer = host + ':' + port
+    if exiting_peer in peer_list:
+        peer_list.remove(exiting_peer)
+        print(peer_list)
+    client_socket.send('Bye'.encode())
+
+# handle every bad request from a client
+def client_badrequest(data_list, client_socket, data):
+    pass
+
+
+def new_connection(client_socket):
+    data = client_socket.recv(1024).decode()
+    print('new request from client')
+    data_list = data.split('\n')
+    for line in data_list:
+        print(line)
+    if data_list[0].split(' ')[0] == 'JOIN':
+        client_join(data_list, client_socket)
+    elif data_list[0].split(' ')[0] == 'EXIT':
+        client_exit(data_list, client_socket)
+    else:
+        client_badrequest(data_list, client_socket, data)
+
+
+# main functionality of the central server
+server_socket = socket.socket()  # Create a socket object
+# host = socket.gethostname() # Get local machine name
+# server_host = socket.gethostbyname(socket.gethostname())
+server_host = input('Enter IP address:')
+port = 7734  # Reserve a port for your service.
+server_socket.bind((server_host, port))  # Bind to the port
+print('central server host is ', server_host)
+print('central server port is ', port)
+
+server_socket.listen(5)  # Now wait for client connection.
+print("Central server is awaiting a connection")
+while (True):
+    client_socket, client_addr = server_socket.accept()  # Establish connection with client.
+    print('Got connection from', client_addr)
+    new_thread = Thread(target=new_connection, args=(client_socket,))
+    new_thread.start()
+print('shutting down central server')
+server_socket.close()  # Close the connection
