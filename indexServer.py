9c0b64793dccdc27935655df1ef331efbc8430db
SyerraRyan <sryan19@students.desu.edu>
2022-03-28 14:00:47 -0400

    Add files via upload

--- indexServer.py
+++ indexServer.py
@@ -11,62 +11,86 @@ class P2P:
 
 
 # Client joining the network
-def unpackJoin(messageData, clientSocket, peerID):
-    message = messageData[2:].decode()
-    peerInfo = message[1:]
-    print(peerInfo)
+def clientJoin(dataList, clientSocket, peerID):
     # Get IP and port from client
-    host = peerInfo.split(' ')[0]
-    port = peerInfo.split(' ')[1]
+    host = dataList[1].split(':')[1]
+    port = dataList[2].split(':')[1]
 
     # List for new client information
     clientInfo = [host, port, "Absent"]
     P2P.activePeers[peerID] = clientInfo
     print("Current Index Server: ")
     print(P2P.activePeers)
-    provideActivePeers(clientSocket)
+
+    # Convert dictionary of active peers to a byte string
+    serializedActivePeers = pickle.dumps(P2P.activePeers)
+    # Send the string
+    clientSocket.send(serializedActivePeers)
+    clientSocket.close()
 
 
 # Send active peers dictionary to the requesting client
-def provideActivePeers(clientSocket):
+def provideList(dataList, clientSocket):
     # Get IP and port from client
-    #host = messageData[1].split(':')[1]
-    #port = messageData[2].split(':')[1]
+    host = dataList[1].split(':')[1]
+    port = dataList[2].split(':')[1]
 
     print("Sending active peers list...")
 
-    messageType = 'L'.encode()
     # Convert dictionary of active peers to a byte string
     serializedActivePeers = pickle.dumps(P2P.activePeers)
-    message = messageType + serializedActivePeers
+    # Send the string
+    clientSocket.send(serializedActivePeers)
+    clientSocket.close()
+
+# Get attendance status from client and then pack it
+def packAttendanceStatus(dataList, clientSocket):
+    # encode the message type
+    messageType = 'S'.encode()
+
+    # get attendance status
+    attendance = dataList[3].split(':')[1]
+
+    attendance = attendance.encode()
+
+    # append message type to attendance
+    message = messageType + attendance
+
+    # get the length of the message
     messageSize = len(message)
 
-    message = messageSize.to_bytes(2, 'little') + message
-    # Send the string
+    #convert message to bytes
+    message = messageSize.to_bytes(2,"little")+ message
+
     clientSocket.send(message)
+
     clientSocket.close()
+
+#unpack attendance status
+def unpackAttendanceStatus(message):
+
+    message = message[2:].decode()
+    attendance = message[1:]
+    print(attendance)
 
 
 # Handle new requests from clients
 def newConnection(clientSocket):
     # Received a message from the client
-    message = clientSocket.recv(1024)
+    message = clientSocket.recv(1024).decode()
     print("New request from client")
-    messageContent = message[2:]
-    messageType = messageContent[:1]
-    # Determine the type of message received from the client
-    if (messageType == b'J'):
-        unpackJoin(message, clientSocket, P2P.peerID)
-        P2P.peerID += 1
+    messageList = message.split('\n')
     # print(messageList)
-
-    # for line in messageList:
-    #     print(line)
-    # if messageList[0].split(' ')[0] == 'J':
-    #     clientJoin(messageList, clientSocket, P2P.peerID)
-    #     P2P.peerID += 1
-    # elif messageList[0].split(' ')[0] == 'R':
-    #     provideActivePeers(messageList, clientSocket)
+    # Determine the type of message received from the client
+    for line in messageList:
+        print(line)
+    if messageList[0].split(' ')[0] == 'J':
+        clientJoin(messageList, clientSocket, P2P.peerID)
+        P2P.peerID += 1
+    elif messageList[0].split(' ')[0] == 'R':
+        provideList(messageList, clientSocket)
+    elif messageList[0].split(' ')[0] == 'S':
+        provideList(messageList, clientSocket)
 
 
 # Functionality of centralized server
